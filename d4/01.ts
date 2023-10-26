a45382dbb42a663d69a8a3d281d09ac0a81cc60e
Tomasz Piątek <tomasz.piatek@sts.pl>
2023-10-26 11:00:37 +0200

    Zadania z dnia 4

--- d4/01.ts
+++ d4/01.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 const argv = require('minimist')(process.argv.slice(2));
 
-const openAiAPI = async (system, task) => {
+const openAiAPI = async (task) => {
     const openai = new OpenAI({
         apiKey: Bun.env.OPENAI_KEY
     });
@@ -12,11 +12,11 @@ const openAiAPI = async (system, task) => {
         messages: [
             {
                 "role": "system",
-                "content": `answer truthfully, as briefly as possible. context: ${JSON.stringify(system)}`
+                "content": `answer truthfully, as briefly as possible. ###context: ${JSON.stringify(task.input)}`
             },
             {
                 "role": "user",
-                "content": task
+                "content": task.msg + '. Finally format should looks like: {"answer": [1, 0, 0, 1]}'
             }
         ],
     });
@@ -28,18 +28,18 @@ const openAiAPI = async (system, task) => {
 const { code, token, msg } = await fetch(`https://zadania.aidevs.pl/token/${argv.task}`, {
     method: 'POST',
     body: JSON.stringify({ apikey: Bun.env.API_KEY })
-}).then(r => r.json())
+}).then(r => r.json());
 
 if (code === 0) {
     const task = await fetch(`https://zadania.aidevs.pl/task/${token}`).then(r => r.json());
 
     console.log('task', task);
-    const answer = await openAiAPI(task, task.msg);
+    const answer = await openAiAPI(task);
     console.log(answer);
 
     const sendAnswer = await fetch(`https://zadania.aidevs.pl/answer/${token}`, {
         method: 'POST',
-        body: JSON.stringify({ answer })
+        body: answer
     }).then(r => r.json());
 
     console.log('send answer', sendAnswer);
