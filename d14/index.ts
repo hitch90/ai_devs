091f70001425e95613757ea0fa0f0d3a19f7e9a9
Tomasz Piątek <tomasz.piatek@sts.pl>
2023-11-12 16:59:41 +0100

    Zadanie z dnia 14

--- d14/index.ts
+++ d14/index.ts
@@ -3,17 +3,18 @@ Rozwiąż zadanie o nazwie “whoami”. Za każdym razem, gdy pobierzesz zadani
 */
 // import { task } from '../task-details';
 import 'dotenv/config'
-import { token } from '../token';
+import { getToken } from '../token';
 import { HumanMessage, SystemMessage } from "langchain/schema";
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { v4 as uuidv4 } from 'uuid';
 import { QdrantClient } from '@qdrant/js-client-rest';
+import { task } from '../task-details';
 const argv = require('minimist')(process.argv.slice(2));
 
 const ARCHIVE_URL = 'https://unknow.news/archiwum.json';
 const COLLECTION_NAME = "ai_devs";
-const qdrant = new QdrantClient({ url: 'http://localhost:6333' });
+const qdrant = new QdrantClient({ url: 'http://127.0.0.1:6333' });
 const embeddings = new OpenAIEmbeddings({
     openAIApiKey: process.env.OPENAI_KEY,
     maxConcurrency: 5
@@ -35,72 +36,92 @@ interface Archive {
     }
 }
 
-const main = async () => {
-    const { code, msg } = await token();
+const { code, msg, token } = await getToken();
 
-    // pobranie kolekcji z qdrant, jeśli nie istnieje to ją stwórz.
-    const result = await qdrant.getCollections();
-    const indexed = result.collections.find((collection) => collection.name === COLLECTION_NAME);
-    if (!indexed) {
-        await qdrant.createCollection(COLLECTION_NAME, { vectors: { size: 1536, distance: 'Cosine', on_disk: true } });
-    }
+// pobranie kolekcji z qdrant, jeśli nie istnieje to ją stwórz.
+const result = await qdrant.getCollections();
+const indexed = result.collections.find((collection) => collection.name === COLLECTION_NAME);
+if (!indexed) {
+    await qdrant.createCollection(COLLECTION_NAME, { vectors: { size: 1536, distance: 'Cosine', on_disk: true } });
+}
 
-    const collectionInfo = await qdrant.getCollection(COLLECTION_NAME);
+const collectionInfo = await qdrant.getCollection(COLLECTION_NAME);
 
-    if (!collectionInfo.points_count) {
-        const archiveJson: Archive[] = await fetch(ARCHIVE_URL).then(r => r.json()) as Archive[];
+// dodaj wpisy do bazy jeśli jest pusta
+if (!collectionInfo.points_count) {
+    const archiveJson: Archive[] = await fetch(ARCHIVE_URL).then(r => r.json()) as Archive[];
 
-        // dopisz metadane
-        const archive: any[] = archiveJson.slice(0, 300).map((item: Archive) => ({
-            ...item,
-            metadata: {
-                uuid: uuidv4(),
-                source: COLLECTION_NAME,
-                title: item.title,
-                info: item.info,
-                date: item.date,
-                url: item.url
-            }
-        }));
-
-        // Generate embeddings
-        const points: any[] = [];
-        for (const document of archive) {
-            const [embedding] = await embeddings.embedDocuments([document.title, document.info]);
-            points.push({
-                id: document.metadata.uuid,
-                payload: document.metadata,
-                vector: embedding,
-            });
+    // dopisz metadane
+    const archive: any[] = archiveJson.slice(0, 300).map((item: Archive) => ({
+        ...item,
+        metadata: {
+            uuid: uuidv4(),
+            source: COLLECTION_NAME,
+            title: item.title,
+            info: item.info,
+            date: item.date,
+            url: item.url
         }
+    }));
 
-        await qdrant.upsert(COLLECTION_NAME, {
-            wait: true,
-            batch: {
-                ids: points.map((point) => (point.id)),
-                vectors: points.map((point) => (point.vector)),
-                payloads: points.map((point) => (point.payload)),
-            },
-        })
-
-        console.log('Done! Dane dodane do bazy!');
+    // Generate embeddings
+    const points: any[] = [];
+    for (const document of archive) {
+        const [embedding] = await embeddings.embedDocuments([document.title, document.info]);
+        points.push({
+            id: document.metadata.uuid,
+            payload: document.metadata,
+            vector: embedding,
+        });
     }
 
+    await qdrant.upsert(COLLECTION_NAME, {
+        wait: true,
+        batch: {
+            ids: points.map((point) => (point.id)),
+            vectors: points.map((point) => (point.vector)),
+            payloads: points.map((point) => (point.payload)),
+        },
+    })
 
-    if (code === 0) {
-        const sendAnswer = async (text: string) => {
-            const sendedAnswer = await fetch(`https://zadania.aidevs.pl/answer/${token}`, {
-                method: 'POST',
-                body: JSON.stringify({ answer: text })
-            }).then(r => r.json());
+    console.log('Done! Dane dodane do bazy!');
+}
 
-            console.log(sendedAnswer);
+
+if (code === 0) {
+    const sendAnswer = async (text: string) => {
+        const sendedAnswer = await fetch(`https://zadania.aidevs.pl/answer/${token}`, {
+            method: 'POST',
+            body: JSON.stringify({ answer: text })
+        }).then(r => r.json());
+
+        console.log(sendedAnswer);
+    }
+
+    const taskDetails = await task(token);
+
+    console.log(taskDetails);
+
+    const queryEmbedding = await embeddings.embedQuery(taskDetails.question);
+
+    const search = await qdrant.search(COLLECTION_NAME, {
+        vector: queryEmbedding,
+        limit: 1,
+        filter: {
+            must: [
+                {
+                    key: 'source',
+                    match: {
+                        value: COLLECTION_NAME
+                    }
+                }
+            ]
         }
-
-    } else {
-        console.log(`${code} - Hmm, wystąpił jakiś błąd: 
-    ${msg}`);
+    });
+    if (search.length) {
+        sendAnswer(search[0]?.payload?.url);
     }
-};
-
-main();
+} else {
+    console.log(`${code} - Hmm, wystąpił jakiś błąd: 
+        ${msg}`);
+}
--- token.ts
+++ token.ts
@@ -1,6 +1,6 @@
 const argv = require('minimist')(process.argv.slice(2));
 
-export const token: any = async () => await fetch(`https://zadania.aidevs.pl/token/${argv.task}`, {
+export const getToken: any = async () => await fetch(`https://zadania.aidevs.pl/token/${argv.task}`, {
     method: 'POST',
     body: JSON.stringify({ apikey: process.env.API_KEY })
 }).then(r => r.json());
